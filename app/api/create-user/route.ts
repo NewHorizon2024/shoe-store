7793440ef28d891f39998f4454946bc16022e333
mostafa <mustafa.husseiny@gmail.com>
2025-11-22 20:04:27 +0100

    switch develop

--- app/api/create-user/route.ts
+++ app/api/create-user/route.ts
@@ -1,4 +1,5 @@
 import type { LoginForm } from "@/models/models";
+import bcrypt from "bcrypt";
 import { type NextRequest, NextResponse } from "next/server";
 import { DatabaseError } from "pg";
 
@@ -7,13 +8,17 @@ import { insertUserQuery } from "@/lib/db/queries";
 
 export async function POST(request: NextRequest) {
   const payload = (await request.json()) as Promise<LoginForm>;
+  const values = Object.values(payload);
+  const password = values[3];
+  const hashedPassword = await bcrypt.hash(password, 10);
+  values[3] = hashedPassword;
+
   try {
-    const result = await pool.query(insertUserQuery, Object.values(payload));
+    const result = await pool.query(insertUserQuery, values);
     return NextResponse.json({ response: result.rows }, { status: 201 });
   } catch (error) {
     if (error instanceof DatabaseError) {
       if (error.detail?.indexOf("already exists")) {
-        console.error("Database error:", error.message, error.detail);
         return NextResponse.json({
           error: true,
           reason: "User already exits",
