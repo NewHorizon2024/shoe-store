bff19d41dc58a077099313730c1904eb98cf1b11
mostafa <mustafa.husseiny@gmail.com>
2025-11-23 19:24:35 +0100

    fix

--- app/(main)/products/[slug]/page.tsx
+++ app/(main)/products/[slug]/page.tsx
@@ -10,6 +10,8 @@ import axios from "axios";
 import Image from "next/image";
 import { useParams } from "next/navigation";
 
+import { Button } from "@/components/ui/button";
+
 import ProductPrice from "../../_components/ProductPrice";
 
 export default function Slug() {
@@ -29,12 +31,10 @@ export default function Slug() {
 
   useEffect(() => console.log(data), [data]);
 
-  if (!data?.id) return null
+  if (!data?.id) return null;
   return (
     <div className="flex justify-center px-4">
-      {/* Wrapper with max width */}
       <div className="w-full max-w-5xl flex flex-col md:flex-row md:space-x-8">
-        {/* Left box: product image */}
         <div className="flex-1 flex justify-center items-center bg-gray-100 rounded p-4">
           <Image
             width={200}
@@ -45,11 +45,12 @@ export default function Slug() {
           />
         </div>
 
-        {/* Right box: product details */}
         <div className="flex-1 flex flex-col space-y-4 p-4">
           <h1 className="text-2xl font-bold">{data?.title}</h1>
           <p className="text-gray-600">{data?.description}</p>
           <ProductPrice price={data?.price} />
+
+          <Button className="mt-4 cursor-pointer">Add to cart</Button>
         </div>
       </div>
     </div>
