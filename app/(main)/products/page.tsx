3607e7f18fc306a3e8a90fcdc15ab84dceff4199
mostafa <mustafa.husseiny@gmail.com>
2025-11-24 03:51:17 +0100

    client

--- app/(main)/products/page.tsx
+++ app/(main)/products/page.tsx
@@ -1,23 +1,28 @@
-"use server";
+"use client";
 
 import type { Product } from "@/models/models";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 import ProductCard from "../_components/ProductCard";
 
-async function getProducts(): Promise<{ products: Product[] }> {
-  const data = await fetch("/get-list-products");
-  const response = await data.json();
-  return { products: response };
-}
+export default function Home() {
+  const { data } = useQuery({
+    queryKey: ["LIST_PRODUCTS"],
+    queryFn: async () => {
+      const result = await axios.get("/api/get-list-products");
+      const response = result.data as { products: Product[] };
+      return response;
+    },
+  });
 
-export default async function Home() {
-  const { products } = await getProducts();
+  console.log(data);
 
   return (
     <div className="flex flex-col gap-4">
       <h1 className="pl-40 pt-10">Current trends</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 py-10">
-        {products.map(({ id, image_url, title, price }) => (
+        {data?.products.map(({ id, image_url, title, price }) => (
           <ProductCard key={id} image={image_url} title={title} price={price} />
         ))}
       </div>
